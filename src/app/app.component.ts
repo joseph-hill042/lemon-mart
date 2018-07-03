f84e0bd1421c18841abd4f97cb53f10c56d968f9
Joseph Hill <joseph.hill1@outlook.com>
2018-07-03 12:49:55 -0400

    Added component modules and generated icon set for favicon and brand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { DomSanitizer } from '@angular/platform-browser'
+import { MatIconRegistry } from '@angular/material'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app'
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'lemon',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/lemon.svg')
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,28 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { MaterialModule } from './material.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ManagerModule } from './manager/manager.module'
+import { InventoryModule } from './inventory/inventory.module'
+import { PosModule } from './pos/pos.module'
+import { UserModule } from './user/user.module'
+import { HomeComponent } from './home/home.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { HttpClientModule } from '@angular/common/http'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
     BrowserAnimationsModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+    HttpClientModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
